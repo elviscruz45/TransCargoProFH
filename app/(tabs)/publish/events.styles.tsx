a1377f9f283a286afd97368c946d721b2995829a
Elvis Ronald Cruz Chullo <teseosoftwarecompany@gmail.com>
2025-01-04 09:04:43 -0500

    2

--- app/(tabs)/publish/events.styles.tsx
+++ app/(tabs)/publish/events.styles.tsx
@@ -53,7 +53,7 @@ export const styles = StyleSheet.create({
 
     marginTop: 0,
     borderRadius: 20,
-    borderWidth: 0.6,
+    borderWidth: 0.5,
   },
   textArea: {
     ...(Platform.OS === "web"
